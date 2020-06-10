bba3e9c4b65110412fcac2570f7420813fdaeeda
Nik <47363595+nikolay07@users.noreply.github.com>
2020-06-10 13:08:49 +0300

    1

--- lesson1/task1/src/App.jsx
+++ lesson1/task1/src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ThemedButton from './ThemedButton';
+import ThemedButton from "./ThemedButton";
 
-const App =()=>{
-    return <div><ThemedButton/></div>
-}
+const App = () => {
+  return (
+    <div>
+      <ThemedButton />
+    </div>
+  );
+};
 export default App;
--- lesson1/task1/src/ThemedButton.jsx
+++ lesson1/task1/src/ThemedButton.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const ThemedButton =()=>{
-    return <div>
-        <button className="btn">
-    Dynamic Theme
-  </button>
-  
-  <button className="btn">
-    Default Theme
-  </button></div>
-}
+const ThemedButton = () => {
+  return (
+    <div>
+      <button className="btn">Dynamic Theme</button>
+
+      <button className="btn">Default Theme</button>
+    </div>
+  );
+};
 export default ThemedButton;
