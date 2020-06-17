fa1d924492772d81302c8cb0cfd8f90f3191a8e3
nikolay07 <nikolay0701@ukr.net>
2020-06-17 13:04:57 +0300

    1

--- sessions/task1/src/App.jsx
+++ sessions/task1/src/App.jsx
@@ -1,29 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import UsersList from "./users/UsersList";
-import { connect } from "react-redux";
-import * as usersActions from "./users/users.actions";
 
-const App = ({ usersList, filterText, setFilterText }) => {
-  //const [filterText, setFilterText] = useState("");
-  const usersToDisplay = usersList.filter((user) =>
-    user.name.toLowerCase().includes(filterText)
-  );
-  return (
-    <UsersList
-      usersToDisplay={usersToDisplay}
-      filterText={filterText}
-      setFilterText={setFilterText}
-    />
-  );
+const App = () => {
+  return <UsersList />;
 };
-const mapState = (state) => {
-  return {
-    usersList: state.users.usersList,
-    filterText: state.users.filterText,
-  };
-};
-const mapDispatch = {
-  setFilterText: usersActions.setFilterText,
-};
-export default connect(mapState, mapDispatch)(App);
-//export default App;
+
+export default App;
--- sessions/task1/src/users/UsersList.jsx
+++ sessions/task1/src/users/UsersList.jsx
@@ -1,28 +1,39 @@
-import React, { Component } from "react";
-
+import React from "react";
+import { connect } from "react-redux";
+import * as usersActions from "./users.actions";
 import User from "./User";
 import Filter from "./Filter";
-class UsersList extends Component {
-  handleChange = (event) => {
-    this.props.setFilterText(event.target.value.toLowerCase());
-  };
-  render() {
-    const { usersToDisplay, filterText } = this.props;
-    return (
-      <div>
-        <Filter
-          count={usersToDisplay.length}
-          onChange={this.handleChange}
-          filterText={filterText}
-        />
-        <ul className="users">
-          {usersToDisplay.map((user) => (
-            <User key={user.id} {...user} />
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
 
-export default UsersList;
+const UsersList = ({ usersList, filterText, setFilterText }) => {
+  const handleChange = (event) => {
+    setFilterText(event.target.value.toLowerCase());
+  };
+  const usersToDisplay = usersList.filter((user) =>
+    user.name.toLowerCase().includes(filterText)
+  );
+  return (
+    <div>
+      <Filter
+        count={usersToDisplay.length}
+        onChange={handleChange}
+        filterText={filterText}
+      />
+      <ul className="users">
+        {usersToDisplay.map((user) => (
+          <User key={user.id} {...user} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const mapState = (state) => {
+  return {
+    usersList: state.users.usersList,
+    filterText: state.users.filterText,
+  };
+};
+const mapDispatch = {
+  setFilterText: usersActions.setFilterText,
+};
+export default connect(mapState, mapDispatch)(UsersList);
