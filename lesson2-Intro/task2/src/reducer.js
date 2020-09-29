df5b8b41e55fd6d18e281ec3c65ce62e8823b4dc
nikolay07 <nikolay0701@ukr.net>
2020-09-29 20:06:11 +0300

    React Intro

--- lesson2-Intro/task2/src/reducer.js
+++ lesson2-Intro/task2/src/reducer.js
@@ -1,28 +1,25 @@
-const initialState = {
-    history: [],
-  };
-  
-  export const counterReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case INCREMENT:
-        return {
-          ...state,
-  
-          history: state.history.concat("+1"),
-        };
-      case DECREMENT:
-        return {
-          ...state,
-  
-          history: state.history.concat("-1"),
-        };
-      case RESET:
-        return {
-          ...state,
-  
-          history: [],
-        };
-      default:
-        return state;
-    }
-  };
+import { INCREMENT, DECREMENT, RESET } from "./action"
+
+const initialState = { history: [] };
+
+export const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        history: state.history.concat("+1"),
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        history: state.history.concat("-1"),
+      };
+    case RESET:
+      return {
+        ...state,
+        history: [],
+      };
+    default:
+      return state;
+  }
+};
